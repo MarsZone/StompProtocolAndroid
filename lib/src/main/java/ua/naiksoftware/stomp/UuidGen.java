b87d03935877e1b939d7201986d63a6c156dc5d4
Mars <zm03322161@qq.com>
2023-03-02 22:01:33 +0800

    uuid

--- lib/src/main/java/ua/naiksoftware/stomp/UuidGen.java
+++ lib/src/main/java/ua/naiksoftware/stomp/UuidGen.java
@@ -1,5 +1,6 @@
 package ua.naiksoftware.stomp;
 
+import java.util.UUID;
 public class UuidGen {
     public static String[] chars = new String[] { "a", "b", "c", "d", "e", "f",
             "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
